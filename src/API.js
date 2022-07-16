f5e0042ad9609dc7cf7ae2c324f82a68c7ebb04d
sandhya076 <lavurisandhya76@gmail.com>
2022-07-16 23:33:42 +0530

    first commit

--- src/API.js
+++ src/API.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/";
+const BASE_URL = "https://server-emart1.herokuapp.com/";
 const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
 
 export const publicRequest = axios.create({
