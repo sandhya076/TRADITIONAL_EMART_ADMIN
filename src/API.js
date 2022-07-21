c9a5cc8580fc6c2a784edde47a4b834069ee4c6b
sandhya076 <lavurisandhya76@gmail.com>
2022-07-21 10:06:30 +0530

    commit

--- src/API.js
+++ src/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE_URL = "https://server-emart1.herokuapp.com/";
-const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
+const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyZDFhMDMzZWY0NWE1ZjYwM2Y4NmNiMiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1NzkwNTY4OCwiZXhwIjoxNjU4MTY0ODg4fQ.gKApqu5vOgLOqj7tvfqy3-BjBpuduD8enjgdt_ervcA";
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
